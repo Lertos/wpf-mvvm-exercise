3eff750b7217831df4c47aa051f8dc58c04ba398
Lertos <deetasticgaming@gmail.com>
2024-03-30 00:25:21 -0300

    Adding the thread class basics

---
+++ Models/Thread.cs
@@ -0,0 +1,31 @@
+﻿namespace wpf_mvvm_exercise.Models
+{
+    internal class Thread
+    {
+        public string title { get; }
+        public string description { get; }
+        public DateTime createDate { get; }
+        public User ownedBy { get; }
+        public bool locked { get; private set; }
+        public bool only_admin_visible { get; }
+        public bool only_mod_visible { get; }
+        public List<Post> posts { get; }
+
+        public Thread(string title, string description, User ownedBy) 
+        {
+            this.title = title;
+            this.description = description;
+            this.ownedBy = ownedBy;
+            //Set default values
+            this.locked = false;
+            this.only_admin_visible = false;
+            this.only_mod_visible = false;
+            this.posts = new List<Post>();
+        }
+
+        public void EditThread(Thread thread)
+        {
+            
+        }
+    }
+}
