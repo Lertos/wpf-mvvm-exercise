930e60c82fe34c314a8c950f84356f11b1b0b41c
Lertos <deetasticgaming@gmail.com>
2024-03-31 09:51:48 -0300

    Moving default values

--- Models/Thread.cs
+++ Models/Thread.cs
@@ -6,10 +6,10 @@
         public string description { get; private set; }
         public DateTime createDate { get; }
         public User ownedBy { get; }
-        public bool locked { get; private set; }
-        public bool only_admin_visible { get; }
-        public bool only_mod_visible { get; }
-        public List<Post> posts { get; }
+        public bool locked { get; private set; } = false;
+        public bool only_admin_visible { get; } = false;
+        public bool only_mod_visible { get; } = false;
+        public List<Post> posts { get; } = new List<Post>();
 
         public Thread(string title, string description, User ownedBy) 
         {
@@ -17,11 +17,6 @@
             this.description = description;
             this.createDate = DateTime.Now;
             this.ownedBy = ownedBy;
-            //Set default values
-            this.locked = false;
-            this.only_admin_visible = false;
-            this.only_mod_visible = false;
-            this.posts = new List<Post>();
         }
 
         public void EditTitle(string title) => this.title = title;
