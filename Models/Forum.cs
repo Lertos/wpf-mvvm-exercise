3ca7c103cbbaffc37ce2f484b8355be98d861846
Lertos <deetasticgaming@gmail.com>
2024-03-31 14:12:18 -0300

    Getting foundation for Forum done

--- Models/Forum.cs
+++ Models/Forum.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace wpf_mvvm_exercise.Models
+﻿namespace wpf_mvvm_exercise.Models
 {
     internal class Forum
     {
+        private const int MaxTitleLength = 60;
+
+        private string title;
+        public List<Category> categories { get; } = new List<Category>();
+        public List<User> users { get; } = new List<User>();
+
+        public Forum(string title)
+        {
+            this.title = title;
+        }
+
+        public string Title
+        {
+            get { return title; }
+            set { title = value.Substring(0, MaxTitleLength); }
+        }
+
+        //TODO: Implement Add / Delete methods for Categories
+        //TODO: Implement Add / Delete methods for Users
     }
 }
