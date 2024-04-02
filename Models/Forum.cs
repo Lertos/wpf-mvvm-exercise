e4273e8259cf327c17cd457e9cf63f05c5e935c8
Lertos <deetasticgaming@gmail.com>
2024-04-02 18:35:48 -0300

    Adding remaining methods for the Forum class

--- Models/Forum.cs
+++ Models/Forum.cs
@@ -1,4 +1,6 @@
-﻿namespace wpf_mvvm_exercise.Models
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace wpf_mvvm_exercise.Models
 {
     internal class Forum
     {
@@ -19,7 +21,54 @@
             set { title = value.Substring(0, MaxTitleLength); }
         }
 
-        //TODO: Implement Add / Delete methods for Categories
-        //TODO: Implement Add / Delete methods for Users
+        public bool AddCategory(Category category)
+        {
+            foreach (var item in categories)
+            {
+                if (item.Title.ToLower().Equals(item.Title.ToLower()))
+                    return false;
+            }
+
+            categories.Add(category);
+            return true;
+        }
+
+        public bool DeleteCategory(Category category)
+        {
+            foreach (var item in categories)
+            {
+                if (this.Equals(item))
+                {
+                    categories.Remove(category);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool AddUser(User user)
+        {
+            foreach (var item in users)
+            {
+                if (user.LoginName.ToLower().Equals(item.LoginName.ToLower()) || user.DisplayName.ToLower().Equals(item.DisplayName.ToLower()))
+                    return false;
+            }
+
+            users.Add(user);
+            return true;
+        }
+
+        public bool DeleteUser(User user)
+        {
+            foreach (var item in users)
+            {
+                if (this.Equals(item))
+                {
+                    users.Remove(user);
+                    return true;
+                }
+            }
+            return false;
+        }
     }
 }
