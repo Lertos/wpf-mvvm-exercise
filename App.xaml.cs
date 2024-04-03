e4af0292b6a8893e928c48e86eac5f74704b7a1e
Lertos <deetasticgaming@gmail.com>
2024-04-03 09:02:44 -0300

    Testing the models so far

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using wpf_mvvm_exercise.Models;
 
 namespace wpf_mvvm_exercise
 {
@@ -9,6 +10,30 @@ namespace wpf_mvvm_exercise
     /// </summary>
     public partial class App : Application
     {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            User user1 = new (1, "jackson", "123pass", "Jack Son", Role.MEMBER);
+            User user2 = new (2, "anthoney", "pass123", "Ant Honey", Role.MOD);
+
+            Post post1 = new ("I think this website is quite unique!", user1);
+            Post post2 = new ("This website seems more like a tutorial for someone to learn something new...", user2);
+
+            Models.Thread thread = new ("Hot Takes", "A place for all your wildest takes.", user1);
+
+            Category category = new("Members Lounge", "A place for members to dicuss topics.");
+
+            Forum forum = new("WPF Forums");
+
+            thread.AddPost(post1);
+            thread.AddPost(post2);
+
+            category.AddNewThread(thread);
+
+            forum.AddCategory(category);
+
+            base.OnStartup(e);
+        }
     }
 
 }
