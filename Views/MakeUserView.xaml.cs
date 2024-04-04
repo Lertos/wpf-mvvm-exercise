ed50f05594625cdcb40e5f861c33197937ad7c9d
Lertos <deetasticgaming@gmail.com>
2024-04-04 18:21:12 -0300

    Adding the View folder and first view

---
+++ Views/MakeUserView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace wpf_mvvm_exercise.Views
+{
+    /// <summary>
+    /// Interaction logic for MakeUserView.xaml
+    /// </summary>
+    public partial class MakeUserView : UserControl
+    {
+        public MakeUserView()
+        {
+            InitializeComponent();
+        }
+    }
+}
