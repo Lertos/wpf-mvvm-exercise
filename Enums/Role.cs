009969dd8bc197b588cf4f183d19dd475b6198e5
Lertos <deetasticgaming@gmail.com>
2024-04-05 19:31:28 -0300

    Moving Role to its own class

---
+++ Enums/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace wpf_mvvm_exercise.Enums
+{
+    public enum Role
+    {
+        [Description("Member")]
+        MEMBER,
+        [Description("Moderator")]
+        MOD,
+        [Description("Admin")]
+        ADMIN
+    }
+}
