3e399255d45d159779a14f8defe06329081ab9b6
Lertos <deetasticgaming@gmail.com>
2024-04-09 20:06:55 -0300

    Adding the user list page view model

---
+++ ViewModels/UserListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using wpf_mvvm_exercise.Enums;
+using wpf_mvvm_exercise.Models;
+
+namespace wpf_mvvm_exercise.ViewModels
+{
+    public class UserListViewModel : ViewModelBase
+    {
+
+        private readonly ObservableCollection<UserViewModel> _users;
+
+        public IEnumerable<UserViewModel> Users => _users;
+
+        public ICommand CreateUserCommand { get; }
+
+        public UserListViewModel()
+        {
+            _users = new ObservableCollection<UserViewModel>();
+
+            //TODO: Remove - simply for testing
+            _users.Add(new UserViewModel(new User(1, "jackson", "123pass", "Jack Son", Role.MEMBER)));
+            _users.Add(new UserViewModel(new User(2, "anthoney", "pass123", "Ant Honey", Role.MOD)));
+        }
+    }
+}
---
+++ ViewModels/UserViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using wpf_mvvm_exercise.Models;
+
+namespace wpf_mvvm_exercise.ViewModels
+{
+    public class UserViewModel : ViewModelBase
+    {
+        private readonly User _user;
+
+        public int ID => _user.id;
+        public string Username => _user.LoginName;
+        public string DisplayName => _user.DisplayName;
+        public string Role => _user.role.ToString();
+        public int Upvotes => _user.upvotes;
+        public int Downvotes => _user.downvotes;
+
+        internal UserViewModel(User user)
+        {
+            _user = user;
+        }
+    }
+}
