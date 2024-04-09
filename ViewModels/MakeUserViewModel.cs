29468002d7d824edc25eaf0093b6b96623e02f78
Lertos <deetasticgaming@gmail.com>
2024-04-09 20:06:00 -0300

    Adding the view model for user creation

---
+++ ViewModels/MakeUserViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using wpf_mvvm_exercise.Enums;
+
+namespace wpf_mvvm_exercise.ViewModels
+{
+    public class MakeUserViewModel : ViewModelBase
+    {
+        private string _loginName;
+        public string LoginName
+        {
+            get { return _loginName; }
+            set
+            {
+                _loginName = value;
+                OnPropertyChanged(nameof(LoginName));
+            }
+        }
+
+        private string _displayName;
+        public string DisplayName
+        {
+            get { return _displayName; }
+            set
+            {
+                _displayName = value;
+                OnPropertyChanged(nameof(DisplayName));
+            }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged(nameof(Password));
+            }
+        }
+
+        private string _confirmPassword;
+        public string ConfirmPassword
+        {
+            get { return _confirmPassword; }
+            set
+            {
+                _confirmPassword = value;
+                OnPropertyChanged(nameof(ConfirmPassword));
+            }
+        }
+
+        private Role _role;
+        public Role Role
+        {
+            get { return _role; }
+            set
+            {
+                if (_role != value)
+                {
+                    _role = value;
+                    OnPropertyChanged(nameof(Role));
+                }
+            }
+        }
+
+        public ICommand SubmitCommand { get; }
+        public ICommand CancelCommand { get; }
+    }
+}
