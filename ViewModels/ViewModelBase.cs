ef5e514a1eb6d39393c8490a3e2a81062c49f1ee
Lertos <deetasticgaming@gmail.com>
2024-04-09 20:05:07 -0300

    Adding as view model base

---
+++ ViewModels/ViewModelBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_mvvm_exercise.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
