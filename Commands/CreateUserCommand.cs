6f49053c05a3ebf3811fc0a8a25abb71ef3d2174
Lertos <deetasticgaming@gmail.com>
2024-04-10 18:58:40 -0300

    Creating the submit command for our user

---
+++ Commands/CreateUserCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using wpf_mvvm_exercise.Models;
+using wpf_mvvm_exercise.ViewModels;
+
+namespace wpf_mvvm_exercise.Commands
+{
+    public class CreateUserCommand : CommandBase
+    {
+        private readonly Forum forum;
+        private readonly MakeUserViewModel makeUserViewModel;
+
+        public CreateUserCommand(MakeUserViewModel makeUserViewModel, Forum forum)
+        {
+            this.makeUserViewModel = makeUserViewModel;
+            this.forum = forum;
+        }
+
+        public override void Execute(object? parameter)
+        {
+            User user = new User(
+                makeUserViewModel.LoginName,
+                makeUserViewModel.Password,
+                makeUserViewModel.DisplayName,
+                makeUserViewModel.Role
+                );
+
+            forum.AddUser( user );
+        }
+    }
+}
