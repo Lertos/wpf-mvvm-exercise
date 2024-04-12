96bd3812ad803b5d072503895e7a20e57d95f966
Lertos <deetasticgaming@gmail.com>
2024-04-12 19:03:38 -0300

    Adding navigation for later

---
+++ Commands/NavigationCommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_mvvm_exercise.Commands
+{
+    public class NavigationCommand : CommandBase
+    {
+        public override void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
