65a568e5e7a5853ef081de902026e4dcdcbb5fe0
Lertos <deetasticgaming@gmail.com>
2024-04-13 17:26:10 -0300

    Adding service to encapsulate navigation logic

--- Commands/NavigationCommand.cs
+++ Commands/NavigationCommand.cs
@@ -3,14 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using wpf_mvvm_exercise.Models;
+using wpf_mvvm_exercise.Services;
+using wpf_mvvm_exercise.Stores;
+using wpf_mvvm_exercise.ViewModels;
 
 namespace wpf_mvvm_exercise.Commands
 {
     public class NavigationCommand : CommandBase
     {
+        private readonly NavigationService navigationService;
+
+        public NavigationCommand(NavigationService navigationService)
+        {
+            this.navigationService = navigationService;
+        }
+
         public override void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            navigationService.Navigate();
         }
     }
 }
